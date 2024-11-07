97c4b55c144dfe181d1a139344a752eeeab925e0
Sasan Hezarkhani <sasan@useswype.com>
2024-11-07 12:12:40 -0800

    Package: Add npmignore and some cleanup (#3)

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
-import svgr from '@svgr/rollup'
+import dts from 'rollup-plugin-dts';
+import svgr from '@svgr/rollup';
 
 /** @type {import('rollup').RollupOptions} */
-const config = {
+const config = [
+  {
     input: 'src/index.ts',
-    output: {
+    output: [
+      {
         dir: 'output',
         format: 'es',
-    },
+        sourcemap: true,
+      },
+    ],
     plugins: [typescript({ tsconfig: './tsconfig.json' }), svgr()],
-    external: ['react', 'react-dom', 'react/jsx-runtime']
-};
+    external: ['react', 'react-dom', 'react/jsx-runtime'],
+  },
+  {
+    input: 'output/types/index.d.ts',
+    output: {
+      file: 'output/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
 
 export default config;
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,4 @@
 declare module '*.svg' {
-  const content: React.FC<React.SVGProps<SVGSVGElement>>;;
+  const content: React.FC<React.SVGProps<SVGSVGElement>>;
   export default content;
-} 
+}
