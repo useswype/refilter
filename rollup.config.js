f6dffa1de1584bea47b0d863820799434da90da5
Omar Ayman <se.omarayman@gmail.com>
2026-08-05 13:51:44 +0300

    Chore: Add support for react 19 and remove outdated package

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import svgr from '@svgr/rollup';
-import postcss from "rollup-plugin-postcss";
 import nodeResolve from "@rollup/plugin-node-resolve";
 
 import packageJson from './package.json' with { type: 'json' };
@@ -24,21 +23,8 @@ const config = [
         banner,
       },
     ],
-    plugins: [typescript({ tsconfig: './tsconfig.json' }), svgr(), nodeResolve({ resolveOnly: ['tailwind-merge', '@headlessui/react', 'qs'] }), postcss({
-      extract: true,
-      minimize: true,
-    })],
+    plugins: [typescript({ tsconfig: './tsconfig.json' }), svgr(), nodeResolve({ resolveOnly: ['tailwind-merge', '@headlessui/react', 'qs'] })],
     external: ['react', 'react-dom', 'react/jsx-runtime'],
-  },
-  {
-    input: "src/main.css",
-    output: [{ file: 'output/main.css', }],
-    plugins: [
-      postcss({
-        extract: true,
-        minimize: true,
-      }),
-    ],
   },
   {
     input: 'output/types/index.d.ts',
